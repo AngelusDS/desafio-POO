35dfb2e534b5826c370bef2b08de3d3bdb2a232c
AngelusDS <angelo.rodrigues@ufpe.br>
2022-11-12 00:29:01 -0300

    Desafio Abstraindo um bootcamp usando POO em Java. Mentoria da professora Camila Cavalcante.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-import br.com.dio.desafio.Curso;
-import br.com.dio.desafio.Mentoria;
+import br.com.dio.desafio.*;
 
 import java.time.LocalDate;
 
@@ -22,8 +21,56 @@ public class Main {
         mentoria.setData(LocalDate.now());
 
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria);
+//        System.out.println(curso1);
+//        System.out.println(curso2);
+//        System.out.println(mentoria);
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devAngelo = new Dev();
+        devAngelo.setNome("Angelo");
+        devAngelo.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Angelo: " + devAngelo.getConteudosInscritos());
+        devAngelo.progredir();
+        devAngelo.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos Angelo: " + devAngelo.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Angelo: " + devAngelo.getConteudosConcluidos());
+        System.out.println("XP: " + devAngelo.calcularTotalXp());
+
+
+        System.out.println("------");
+
+
+        Dev devPriscila = new Dev();
+        devPriscila.setNome("Priscila");
+        devPriscila.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Priscila: " + devPriscila.getConteudosInscritos());
+        devPriscila.progredir();
+        devPriscila.progredir();
+        devPriscila.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos Priscila: " + devPriscila.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Priscila: " + devPriscila.getConteudosConcluidos());
+        System.out.println("XP: " + devPriscila.calcularTotalXp());
+
+
+        System.out.println("------");
+
+
+        Dev devFeca = new Dev();
+        devFeca.setNome("Fábio");
+        devFeca.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Fábio: " + devFeca.getConteudosInscritos());
+        devFeca.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos Fábio: " + devFeca.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Fábio: " + devFeca.getConteudosConcluidos());
+        System.out.println("XP: " + devFeca.calcularTotalXp());
     }
 }
